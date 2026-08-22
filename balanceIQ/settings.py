01ddb83db27b51ebc1903332b99728ef58fafa5d
ahmadkh511 <32079250+ahmadkh511@users.noreply.github.com>
2026-08-22 13:40:47 +0300

    SITE OUT OF  GITIGNORE

--- balanceIQ/settings.py
+++ balanceIQ/settings.py
@@ -371,9 +371,3 @@ DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'admin@balanceiqsoft.com')
 # CSP_FONT_SRC = ("'self'", "https://fonts.gstatic.com")
 
 
-
-# ==========================================
-# ✅ تم الرفع إلى الاستضافة بتاريخ: 2026-08-21
-# ✅ آخر تعديل: إضافة ALLOWED_HOSTS وإعدادات الأمان
-# ✅ تمت المراجعة بواسطة: (اسمك)
-# ==========================================
